7cdcecafd66cce4e9cb179bf460d1aaa9cc6455e
Kamal Ezzarmou <kamal.ezzarmou1@gmail.com>
2023-12-04 23:36:45 +0100

    job

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,5 +1,17 @@
+import axios from "axios";
 import React from "react";
 
 export default function Jobs() {
+  const jobs = async () => {
+    try {
+      const response = await axios.get("http://localhost:8888/api/job");
+
+      console.log(response);
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error(error);
+    }
+  };
+
   return <div>Jobs</div>;
 }
